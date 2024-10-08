7256c5037e84bcb1d36923bd5ca5d1d189309c8f
Sudip Khatri <sudip.khatri@nectardigit.com>
2024-10-08 10:25:07 +0545

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
-import HomeHero from "../components/homeHero/homeHero";
+import React, { useState } from "react";
+
 import OurServices from "../components/ourServices/OurServices";
 import HomeAbout from "../components/homeAbout/HomeAbout";
-import HomeBlog from "../components/homeblog/HomeBlog";
 import HomeCounters from "../components/homeCounters/HomeCounters";
 import Faq from "../components/faq/Faq";
-import VideoSection from "../components/videoSection/VideoSection";
+
 import ClientList from "../components/clientsList/ClientList";
 import ParticlesBackground from "../components/particles/ParticlesBackground";
 import NewContact from "../components/newContact/NewContact";
