53cc9bd353aa7db966820f2fa6c9db9999e5717c
Sudip Khatri <sudip.khatri@nectardigit.com>
2024-10-08 12:55:12 +0545

    Error Fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,15 +2,14 @@ import PageHero from "../components/pageHero/PageHero";
 import ContactDetail from "../components/contactDetail/ContactDetail";
 import ContactForm from "../components/contactForm/ContactForm";
 import NewContact from "../components/newContact/NewContact";
-
-import CompanyMap from "../components/companyMap/CompanyMap";
+import CompanyLocationMap from "../components/companyLocationMap/CompanyLocationMap";
 
 function Contact() {
   return (
     <main className="contact">
       <PageHero imgSrc="/images/contact-banner-01.png" pageName="contact" />
       <NewContact />
-      <CompanyMap />
+      <CompanyLocationMap />
     </main>
   );
 }
